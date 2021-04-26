03c49c2ac0520c5157be0f47ac410da5d8c7f497
Diogo Teixeira <diogo_alex9@hotmail.com>
2021-04-26 11:14:46 +0100

    FarAway Scene

---
+++ Assets/Scripts/FarAwayController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class FarAwayController : MonoBehaviour
+{
+    public void OpenDirections()
+    {
+        Application.OpenURL("https://goo.gl/maps/GptfUPAtSgxsjN4t7");
+    }
+}
