aea9f39d4607ced6a58ac909b5f7723a6a934a5c
David <dvdguarda@hotmail.com>
2021-05-04 16:32:04 +0100

    Ar core duplicado fixed

--- Assets/Scripts/RealidadeAumentada_SCRIPTS/TrailScript.cs
+++ Assets/Scripts/RealidadeAumentada_SCRIPTS/TrailScript.cs
@@ -2,16 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using GoogleARCore;
+//using GoogleARCore;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-
+/*
 #if UNITY_EDITOR
     // Set up touch input propagation while using Instant Preview in the editor.
     using Input = GoogleARCore.InstantPreviewInput;
 #endif
-
+*/
 
 public class TrailScript : MonoBehaviour
 {
